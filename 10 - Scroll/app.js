d42d99001c46ce6efcaf5f20e6870921c8f6d104
Peter <Posoo617@gmail.com>
2020-08-04 09:53:22 +0300

    scroll project progress -changes to index.html & app.js

--- 10 - Scroll/app.js
+++ 10 - Scroll/app.js
@@ -4,8 +4,27 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById("date")
+date.innerHTML = new Date().getFullYear()
 
 // ********** close links ************
+
+const navToggle = document.querySelector(".nav-toggle")
+const linksContainer = document.querySelector(".links-container")
+const links = document.querySelector(".links")
+
+// navToggle.addEventListener("click", () => {
+// 	linksContainer.classList.toggle("show-links")
+// })
+
+// place the links dynamically
+
+navToggle.addEventListener("click", () => {
+	const containerHeight = linksContainer.getBoundingClientRect().height
+	// console.log(containerHeight)
+	const linksHeight = links.getBoundingClientRect().height
+	console.log(linksHeight)
+})
 
 // ********** fixed navbar ************
 
