e58c88454cbb57725a223d5aec97785c28b2e243
Peter <Posoo617@gmail.com>
2020-08-06 09:05:22 +0300

    scroll - add dynamic links navbar

--- 10 - Scroll/app.js
+++ 10 - Scroll/app.js
@@ -23,10 +23,60 @@ navToggle.addEventListener("click", () => {
 	const containerHeight = linksContainer.getBoundingClientRect().height
 	// console.log(containerHeight)
 	const linksHeight = links.getBoundingClientRect().height
-	console.log(linksHeight)
+	// console.log(linksHeight)
+	if (containerHeight === 0) {
+		//default setup
+		// dynamically add height
+		linksContainer.style.height = `${linksHeight}px `
+	} else {
+		linksContainer.style.height = 0
+	}
 })
 
+const navbar = document.getElementById("nav")
+const topLink = document.querySelector(".top-link")
 // ********** fixed navbar ************
+// use scroll event
+
+window.addEventListener("scroll", () => {
+	// console.log(window.pageYOffset)
+	const scrollHeight = window.pageYOffset
+	const navHeight = navbar.getBoundingClientRect().height
+	if (scrollHeight > navHeight) {
+		navbar.classList.add("fixed-nav")
+	} else {
+		navbar.classList.remove("fixed-nav")
+	}
+	if (scrollHeight > 500) {
+		topLink.classList.add("show-link")
+	} else {
+		topLink.classList.remove("show-link")
+	}
+})
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link")
+
+scrollLinks.forEach(link => {
+	link.addEventListener("click", e => {
+		//prevent default
+		e.preventDefault()
+		//navigate to specific spot
+		const id = e.currentTarget.getAttribute("href").slice(1)
+		// console.log(id)
+		const element = document.getElementById(id)
+
+		//calculate heights
+
+		let position = element.offsetTop
+		// console.log(position)
+
+		window.scrollTo({
+			left: 0,
+			top: position,
+		})
+
+		linksContainer.style.height = 0
+	})
+})
