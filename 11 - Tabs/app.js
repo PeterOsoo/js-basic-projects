5a0dfabdef30a3d822c9f0a24fbc5b62ba676596
Peter <Posoo617@gmail.com>
2020-08-08 10:02:03 +0300

    11. tabs project - event bubbling and datasets

---
+++ 11 - Tabs/app.js
@@ -0,0 +1,26 @@
+const btns = document.querySelectorAll(".tab-btn")
+const about = document.querySelector(".about")
+const articles = document.querySelectorAll(".content")
+
+about.addEventListener("click", e => {
+	// console.log(e.target)
+	// console.log(e.target.dataset.id)
+
+	const id = e.target.dataset.id
+	if (id) {
+		//remove active from other buttons
+		btns.forEach(btn => {
+			btn.classList.remove("active")
+			e.target.classList.add("active")
+		})
+
+		//for content - hide other articles
+		articles.forEach(article => {
+			article.classList.remove("active")
+		})
+		const element = document.getElementById(id)
+		element.classList.add("active")
+	}
+})
+
+// event bubbling and target
