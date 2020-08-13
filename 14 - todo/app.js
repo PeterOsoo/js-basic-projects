e694695064df984f172e3ee6fd121469287d93a6
Peter <Posoo617@gmail.com>
2020-08-13 16:07:30 +0300

    changes to index.html and select elements in app.js

--- 14 - todo/app.js
+++ 14 - todo/app.js
@@ -1,6 +1,21 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert")
+const form = document.querySelector(".glocery-form")
+const grocery = document.getElementById("grocery")
+
+const submitBtn = document.querySelector(".submit-btn")
+
+const container = document.querySelector(".grocery-container")
+const clearBtn = document.querySelector(".clear-btn")
+const list = document.querySelector(".grocery-list")
 
 // edit option
+
+// 3 variables
+
+let editElement
+let editFlag = false
+let editID = ""
 
 // ****** EVENT LISTENERS **********
 
