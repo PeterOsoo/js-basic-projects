f8cca94ea42dc8e7091938c798479a9f980352cb
Peter <Posoo617@gmail.com>
2020-08-14 20:59:14 +0300

    changes to app.js and index.html

--- 14 - todo/app.js
+++ 14 - todo/app.js
@@ -1,6 +1,6 @@
 // ****** SELECT ITEMS **********
 const alert = document.querySelector(".alert")
-const form = document.querySelector(".glocery-form")
+const form = document.querySelector(".grocery-form")
 const grocery = document.getElementById("grocery")
 
 const submitBtn = document.querySelector(".submit-btn")
@@ -18,9 +18,25 @@ let editFlag = false
 let editID = ""
 
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener("submit", addItem)
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+	e.preventDefault()
+	const value = grocery.value
+	// console.log(value)
+	const id = new Date().getTime().toString()
+	// console.log(id)
 
+	if (value !== "" && editFlag === false) {
+		console.log("add item to the list")
+	} else if (value !== "" && editFlag === true) {
+		console.log("editing")
+	} else {
+		console.log("empty value")
+	}
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
