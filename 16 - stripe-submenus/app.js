2922d74c0c47a146d7f46f81a9ca8f5c0e7814e2
Peter <Posoo617@gmail.com>
2020-09-06 15:02:08 +0300

    stripe menu - target sublinks on specific page

--- 16 - stripe-submenus/app.js
+++ 16 - stripe-submenus/app.js
@@ -4,39 +4,82 @@ const toggleBtn = document.querySelector(".toggle-btn")
 const closeBtn = document.querySelector(".close-btn")
 const sidebarWrapper = document.querySelector(".sidebar-wrapper")
 const sidebar = document.querySelector(".sidebar-links")
-
 const linkBtns = [...document.querySelectorAll(".link-btn")]
 const submenu = document.querySelector(".submenu")
 const hero = document.querySelector(".hero")
 const nav = document.querySelector(".nav")
-
-// hide/show sidebar
-
+// hide/show sideabar
 toggleBtn.addEventListener("click", () => {
-	console.log("button toggle")
 	sidebarWrapper.classList.add("show")
 })
-
 closeBtn.addEventListener("click", () => {
 	sidebarWrapper.classList.remove("show")
 })
 
 // set sidebar
-
 sidebar.innerHTML = sublinks
 	.map(item => {
 		const { links, page } = item
-		return `<article> 
-    <h4> ${page} </h4>
-    <div class="sidebar-links"> 
-    ${links
-			.map(link => {
-				return `<a href="${link.url}"> 
-        <i class="${link.icon}"></i> ${link.label}
-        </a>`
-			})
-			.join("")}
-    </div>
-    </article>`
+		return `<article >
+<h4>${page}</h4>
+<div class="sidebar-sublinks">
+${links
+	.map(link => {
+		return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`
+	})
+	.join("")}
+</div>
+</article>`
 	})
 	.join("")
+
+linkBtns.forEach(btn => {
+	btn.addEventListener("mouseover", e => {
+		// specific sublinks
+		const text = e.currentTarget.textContent
+		const tempBtn = e.currentTarget.getBoundingClientRect()
+		console.log(tempBtn)
+		const center = (tempBtn.left + tempBtn.right) / 2
+		const bottom = tempBtn.bottom - 3
+
+		const tempPage = sublinks.find(link => link.page === text)
+		if (tempPage) {
+			const { page, links } = tempPage
+			submenu.classList.add("show")
+			submenu.style.left = `${center}px`
+			submenu.style.top = `${bottom}px`
+
+			// columns
+			let columns = "col-2"
+			if (links.length === 3) {
+				columns = "col-3"
+			}
+			if (links.length > 3) {
+				columns = "col-4"
+			}
+
+			submenu.innerHTML = `
+			<section> 
+			<h4>${page}</h4>
+			<div class="submenu-center ${columns}">
+			${links
+				.map(link => {
+					return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`
+				})
+				.join("")}
+			</div>
+			</section>
+			`
+		}
+	})
+})
+
+hero.addEventListener("mouseover", e => {
+	submenu.classList.remove("show")
+})
+
+nav.addEventListener("mouseover", e => {
+	if (!e.target.classList.contains("link-btn")) {
+		submenu.classList.remove("show")
+	}
+})
