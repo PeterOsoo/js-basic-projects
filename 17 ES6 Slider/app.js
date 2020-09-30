6f1541ff9586ef766c197f15a793f22910007458
Peter <Posoo617@gmail.com>
2020-09-30 16:29:13 +0300

    startSlider functionality

---
+++ 17 ES6 Slider/app.js
@@ -0,0 +1,60 @@
+import data from "./data.js"
+
+const container = document.querySelector(".slide-container")
+const nextBtn = document.querySelector(".next-btn")
+const prevBtn = document.querySelector(".prev-btn")
+
+// if length is 1 hide buttons
+if (data.length === 1) {
+	nextBtn.style.display = "none"
+	prevBtn.style.display = "none"
+}
+
+// if length is 2, add copies of slides
+let people = [...data]
+if (data.length === 2) {
+	people = [...data, ...data]
+}
+
+container.innerHTML = people
+	.map((person, slideIndex) => {
+		const { img, name, job, text } = person
+
+		let position = "next"
+		if (slideIndex === 0) {
+			position = "active"
+		}
+		if (slideIndex === people.length - 1) {
+			position = "last"
+		}
+		if (data.length <= 1) {
+			position = "active"
+		}
+
+		//return article
+		return `<article class="slide ${position}">
+  <img src=${img} class="img" alt="${name}"/>
+  <h4>${name}</h4>
+  <p class="title">${job}</p>
+  <p class="text">
+   ${text}
+  </p>
+<div class="quote-icon">
+<i class="fas fa-quote-right"></i>
+</div>
+ </article>`
+	})
+	.join("")
+
+const startSlider = type => {
+	console.log("hello world")
+	console.log(type)
+}
+
+nextBtn.addEventListener("click", () => {
+	startSlider()
+})
+
+prevBtn.addEventListener("click", () => {
+	startSlider("prev")
+})
